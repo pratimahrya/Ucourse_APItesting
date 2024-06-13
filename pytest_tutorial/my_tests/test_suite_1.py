ed58361d0fc4646298e9b4cb6a6b58ff434cc1b9
pratimaACHARYA <60184548+pratimahrya@users.noreply.github.com>
2024-06-13 17:16:47 +0545

    first commit

---
+++ pytest_tutorial/my_tests/test_suite_1.py
@@ -0,0 +1,12 @@
+import pytest
+
+
+@pytest.mark.smoke
+def test_login_page_valid_user():
+    print("Login with valid user")
+    print("Function: aaaaaaaaaaaaaaaaa")
+
+
+def test_login_page_incorrect_password():
+    print("Login with wrong password")
+    print("Function: bbbbbbbbbbbbbbbb")
---
+++ pytest_tutorial/my_tests/test_suite_2_with_class.py
@@ -0,0 +1,13 @@
+
+
+
+class TestCheckout(object):
+
+    def test_checkout_as_guest(self):
+        print("checkout as guest")
+        print("Class:111111")
+
+    def test_checkout_with_existing_user(self):
+        print("checkout with existing user")
+        print("Class:222222")
+
